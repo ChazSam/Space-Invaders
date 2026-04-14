c0ca5b4e7c1f09d25bc600f655cc50b2faa83614
ChazSam <129200361+ChazSam@users.noreply.github.com>
2026-04-13 20:49:08 -0500

    added player movement

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,3 +1,10 @@
 class_name Player extends CharacterBody2D
 
+@export var move_speed = 350
+
 @onready var input_component: InputComponent = $InputComponent
+
+func _physics_process(delta: float) -> void:
+	var input_dir = Input.get_axis("move_left","move_right")
+	velocity = transform.x * input_dir * move_speed * delta * 100
+	move_and_slide()
