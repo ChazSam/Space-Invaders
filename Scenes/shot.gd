ba87e23c36829ec1830fc968a1cbafead3bcae56
ChazSam <129200361+ChazSam@users.noreply.github.com>
2026-08-25 20:57:58 -0500

    delete rockets off screen

--- Scenes/shot.gd
+++ Scenes/shot.gd
@@ -3,5 +3,13 @@ extends Node2D
 var speed = 300
 var direction = 1
 
+@onready var visible_notifier = $VisibleNotifier
+
+func _ready() -> void:
+	visible_notifier.connect("screen_exited", _on_screen_exited)
+
 func _process(delta: float) -> void:
 	global_position.y += speed * direction * delta
+
+func _on_screen_exited():
+	queue_free()
